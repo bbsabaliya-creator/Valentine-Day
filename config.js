b14bceb0534b88e42e03f74aaabf345df3e1541a
bbsabaliya-creator <bbsabaliya@gmail.com>
2026-02-03 18:19:27 +0530

    Create config.js

---
+++ config.js
@@ -0,0 +1,20 @@
+const CONFIG = {
+
+  name: "Ayushi 💖",
+
+  questions: [
+    "Do you love me? 😍",
+    "How much do you love me? 💕",
+    "Will you be my Valentine? 💘"
+  ],
+
+  yesMessages: [
+    "I knew it! 😘",
+    "You're my world 💞",
+    "I love you forever 💖"
+  ],
+
+  finalMessage: "Yayyy! You are officially my Valentine 💍💘",
+
+  noMessage: "Hey! You can't say No 😝❤️"
+};
